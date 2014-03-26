68bca32b4b53fe04b91effe9032edd735f7a9f27
FSeror <ferdy89@gmail.com>
2014-03-26 16:18:01 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,65 @@
+/**
+ * Class to encapsulate the main functionality plus the DOM manipulation
+ */
+function App() {
+  this.body = document.getElementsByTagName('body')[0];
+  this.goals = [];
+
+  this.goalElement = document.getElementById('goal');
+
+  // Assign the handler for the form submit
+  document.getElementsByTagName('form')[0].onsubmit = this.addGoalHandler(this);
+
+  // Autofocus to the text area when the page loads
+  this.goalElement.autofocus = true;
+}
+
+/**
+ * Pops up the print dialog on the browser
+ */
+App.prototype.print = function() {
+  window.print();
+}
+
+/**
+ * Simply renders the goals list and puts it into the body of the page
+ */
+App.prototype.renderGoalsTable = function() {
+  var cell = null;
+
+  for (var i = 0; i < 7; i++) {
+    cell = document.getElementById('goal-cell-' + i);
+
+    cell.innerHTML = this.goals[i];
+  }
+}
+
+/**
+ * Metafunction that returns another function with access to a 'that' var which
+ * points to the original object. Probably worth to FIXME
+ *
+ * @param [App] that
+ *
+ * @return [Function]
+ */
+App.prototype.addGoalHandler = function(that) {
+  /**
+   * Handler for the basic form. This will be triggered any time the 'Enter' key
+   * is pressed while focused on the text field
+   */
+  return function() {
+    that.goals.push(that.goalElement.value);
+    that.goalElement.value = "";
+
+    document.getElementById('goals').innerHTML =
+      "<li>" + that.goals.join("</li><li>") + "</li>";
+
+    if (that.goals.length >= 7) {
+      that.renderGoalsTable();
+      document.getElementById('printable-table').className = '';
+      document.getElementById('goal-list').className = 'hidden';
+    }
+
+    return false;
+  }
+}
